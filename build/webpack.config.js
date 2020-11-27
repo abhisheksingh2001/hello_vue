fb774795a82c30f2876a38f137fcf60374d80062
Abhishek <abhishek.new112@gmail.com>
2020-11-27 12:18:50 +0530

    completed basic page

---
+++ build/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: [
+        './src/main.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                    "vue-loader"
+                ],
+            },
+        ],
+    },
+};
